ccc20c45579983166ec6abf3b3b666f6780bc25a
Lauren Piontek <piontekle@gmail.com>
2018-10-23 18:26:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
   ]
   
 def won?(board)
-  WIN_COMBINATIONS.each do |combination|
+  WIN_COMBINATIONS.detect do |combination|
     win_combo = [board[combination[0]], board[combination[1]], board[combination[2]]]
     if win_combo.all? {|play| play == "X" || play == "O"}
      combination
