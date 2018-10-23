9efec6965813f0012f7f398b6d0096850e79e100
Lauren Piontek <piontekle@gmail.com>
2018-10-23 17:42:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # top row
+  [3, 4, 5], # mid row
+  [6, 7, 8], # bot row
+  [0, 3, 6], # left column
+  [1, 4, 7], # mid column
+  [2, 5, 8], # right column
+  [0, 4, 8], # lft-rt diag
+  [2, 4, 6]  # rt-lft diag
+  ]
