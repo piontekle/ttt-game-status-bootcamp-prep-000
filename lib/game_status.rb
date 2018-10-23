4e77e876371cbae17828b57c044755becd77b7c3
Lauren Piontek <piontekle@gmail.com>
2018-10-23 18:14:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,8 +19,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
       if board[win_combination[0]] != 0 && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
         return win_combination
-      else
-        false
       end
+      false
   end
 end
